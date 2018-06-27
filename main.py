00b6b444bf0492055e50e7ee662d9735356810f2
miozune <miozune@gmail.com>
2018-06-28 06:43:02 +0900

    add reporting

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def timed_auto_follow():
     auto_follow.main().main()
 
 
-# @schedule.scheduled_job("cron", hour="0,1,2,3,4,5,6,7,8,9,10,11")
-# def timed_regular_tweet():
-#     regular_tweet.regular_tweet()
-
-
-@schedule.scheduled_job('interval', seconds=70)
+@schedule.scheduled_job("cron", hour="0,1,2,3,4,5,6,7,8,9,10,11")
 def timed_regular_tweet():
     regular_tweet.regular_tweet()
+
+
+# @schedule.scheduled_job('interval', seconds=70)
+# def timed_regular_tweet():
+#     regular_tweet.regular_tweet()
 
 
 if __name__ == '__main__':
