5056fb7a289127581a5dfdebd9cb3053c21c20b3
miozune <miozune@gmail.com>
2018-06-28 07:04:42 +0900

    shortened_text

--- regular_tweet.py
+++ regular_tweet.py
@@ -4,6 +4,7 @@ import tweepy
 from random import choice
 from random import randint
 from Library import *
+from Library.tweet_mochi import shortened_text
 
 auth = get_auth.get_auth()
 api = tweepy.API(auth)
@@ -12,6 +13,7 @@ api = tweepy.API(auth)
 def regular_tweet():
     tweet = ''.join([choice(mochimochi.level1 + mochimochi.level2 + mochimochi.level3 + mochimochi.level4)
                     for _ in range(1, randint(1, 20))])
+    tweet = shortened_text(tweet)
     api.update_status(tweet)
     print('regular tweet success!')
     print(tweet)
