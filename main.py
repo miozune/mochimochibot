5509d8c2bee24aac8dde4c76f7da141a10d19550
miozune <miozune@gmail.com>
2018-06-28 06:22:34 +0900

    TODO:commentout

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+import mochi_stream
+import auto_follow
+import regular_tweet
+
+schedule = BlockingScheduler()
+
+
+@schedule.scheduled_job('interval', seconds=70)
+def timed_auto_follow():
+    auto_follow.main().main()
+
+
+# @schedule.scheduled_job("cron", hour="0,1,2,3,4,5,6,7,8,9,10,11")
+# def timed_regular_tweet():
+#     regular_tweet.regular_tweet()
+
+
+@schedule.scheduled_job('interval', seconds=70)
+def timed_regular_tweet():
+    regular_tweet.regular_tweet()
+
+
+if __name__ == '__main__':
+    mochi_stream.main()
+    schedule.start()
