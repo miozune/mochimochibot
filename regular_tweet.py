f31704359f6bdad9ab9f77964ec50b7a8463c54c
miozune <miozune@gmail.com>
2018-06-28 23:11:54 +0900

    shorter regular twwet

--- regular_tweet.py
+++ regular_tweet.py
@@ -12,7 +12,7 @@ api = tweepy.API(auth)
 
 def regular_tweet():
     tweet = ''.join([choice(mochimochi.level1 + mochimochi.level2 + mochimochi.level3 + mochimochi.level4)
-                    for _ in range(1, randint(1, 20))])
+                    for _ in range(1, randint(1, 15))])
     tweet = shortened_text(tweet)
     api.update_status(tweet)
     print('regular tweet success!')
