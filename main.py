511c240c2516e9422027743196e879f16d982c9e
miozune <miozune@gmail.com>
2018-06-28 17:09:50 +0900

    tweet also in p.m.

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def timed_auto_follow():
     auto_follow.main().main()
 
 
-@schedule.scheduled_job("cron", hour="0,1,2,3,4,5,6,7,8,9,10,11")
+@schedule.scheduled_job("cron", hour="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23")
 def timed_regular_tweet():
-    regular_tweet.regular_tweet()
+    regular_tweet.main()
 
 
 # @schedule.scheduled_job('interval', seconds=70)
