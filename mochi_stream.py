4e9d5bc06a6ae2f2c1f791f68d63f0ef571410fd
miozune <miozune@gmail.com>
2019-02-15 15:20:10 +0900

    [fix] Streaming API廃止に伴いtsaeに置き換えた

--- mochi_stream.py
+++ mochi_stream.py
@@ -1,61 +1,38 @@
 # -*- coding: utf-8 -*-
 
+from tsae import StreamingEmulate
 import tweepy
 from Library import *
 
+list_name = 'mochi'
 auth = get_auth.get_auth()
 api = tweepy.API(auth)
 my_status = api.me()
 
 
-class Listener(tweepy.StreamListener):
-    def on_status(self, status):
-        reply_mochi = tweet_mochi.TweetMochi(status)
+def callback(status):
+    reply_mochi = tweet_mochi.TweetMochi(status)
 
-        if reply_mochi.is_reply_target(my_status):
-            success = False
+    if reply_mochi.is_reply_target(my_status):
+        success = False
 
-            try:
-                api.update_status(reply_mochi.tweet(), status.id)
-            except tweepy.error.TweepError as e:
-                reply_mochi.reply_fail_report(e)
+        try:
+            api.update_status(reply_mochi.tweet(), status.id)
+        except tweepy.error.TweepError as e:
+            reply_mochi.reply_fail_report(e)
 
-            try:
-                api.create_favorite(status.id)
-                success = True
-            except tweepy.error.TweepError as e:
-                reply_mochi.fav_fail_report(e)
+        try:
+            api.create_favorite(status.id)
+            success = True
+        except tweepy.error.TweepError as e:
+            reply_mochi.fav_fail_report(e)
 
-            if success:
-                reply_mochi.reply_and_fav_success_report()
-
-        return True
-
-    def on_error(self, status_code):
-        print('*' * 20)
-        print('ERROR! status code: ' + str(status_code))
-        print('*' * 20)
-
-        with open('Library\dump.txt', 'a') as f:
-            import csv
-            from datetime import datetime
-            writer = csv.writer(f)
-
-            writer.writerow(str(datetime.now()))
-            writer.writerow(str(status_code))
-            writer.writerow('-' * 30)
-
-        if status_code == '420':
-            print('disconnecting...')
-            return False
-
-        return True
+        if success:
+            reply_mochi.reply_and_fav_success_report()
 
 
 def main():
-    listener = Listener()
-    stream = tweepy.Stream(auth, listener, secure=True)
-    stream.userstream(async=True)
+    StreamingEmulate(api, list_name, callback).run()
 
 
 if __name__ == '__main__':
